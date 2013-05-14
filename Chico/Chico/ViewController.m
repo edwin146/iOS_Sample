eed130b6f681291942a457f0708a706e80644ccf
Alireza Mohammadi <alireza@terpmail.umd.edu>
2013-05-13 22:57:01 -0400

    add new samples

--- Chico/Chico/ViewController.m
+++ Chico/Chico/ViewController.m
@@ -18,6 +18,52 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    
+    
+    
+    
+    int userAge = 19 ;
+    int userAgeLimit = 18 ;
+    
+    char userSex = 'G';
+    
+    
+    
+    // Example of if-else statement
+    if (userAge < userAgeLimit ){
+        NSLog(@"No you cannot buy");
+    }else if ( (userAge == userAgeLimit) || (userSex == 'G')  ){
+        NSLog(@"happy 18 !!");
+    }else{
+        NSLog(@"Yes you can buy");
+    }
+    
+    
+    
+    // Example of For loop
+    
+    for (int i=0 ; i<=100; i++ ){
+        NSLog (@"%d",i);
+    }
+    
+    
+    // Example of While loop
+    int i=1;
+    while(i<10){
+        NSLog(@"I'm inside while loop");
+        i++;
+    }
+    
+    // Example of do-while
+    
+    int j = 0;
+    do{
+        NSLog(@"I'm inside do-while loop");
+        j++;
+    }while(j < 10);
+    
+    
 }
 
 - (void)didReceiveMemoryWarning
